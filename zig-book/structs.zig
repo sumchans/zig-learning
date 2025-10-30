2f94d1e3f8965256ff51350aea8b5c2dd27ea1a9
sumchans <sumchan@msn.com>
2025-10-30 12:55:15 -0700

    Structs

--- zig-book/structs.zig
+++ zig-book/structs.zig
@@ -1,9 +1,20 @@
 const std = @import("std");
 
+const User = struct {
+    id: u64,
+    name: []const u8,
+    email: []const u8,
+
+    fn init(id: u64, name: []const u8, email: []const u8) User {
+        return User{ .id = id, .name = name, .email = email };
+    }
+    fn print_name(self: User) !void {
+        std.debug.print("{s}\n", .{self.name});
+        std.debug.print("{s}\n", .{self.email});
+    }
+};
+
 pub fn main() void {
-    const greeting = "hello";
-    var x: []const u8 = "Hello World";
-    x = "Hello Sumesh";
-    std.debug.print("{any}\n", .{@TypeOf(greeting)});
-    std.debug.print("String x is {s}\n", .{x});
+    const u = User.init(1, "Sumesh", "sumchan@msn.com");
+    try u.print_name();
 }
